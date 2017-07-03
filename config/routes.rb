00e9e74ecb3e99dcc121a514f398c63baa03012c
Nina Wilson <nina.wilson.mlis@gmail.com>
2017-07-03 19:12:02 -0400

    set up my routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcom#index'
 
-  get 'params' => 'welcom#params'
+  get 'params' => 'welcom#about_params'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
