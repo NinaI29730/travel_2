9235a80a58d96affafbbd28a328c69b95e201d9a
Nina Wilson <nina.wilson.mlis@gmail.com>
2017-07-03 21:14:49 -0400

    index

--- app/controllers/welcom_controller.rb
+++ app/controllers/welcom_controller.rb
@@ -2,12 +2,15 @@ class WelcomController < ApplicationController
   def index
     @homeland = "Georgia, USA"
     @countries = ['Bahamas', 'Brazil', 'Columbia', 'Costa Rica' ]
-    @travel_pictures = ['costarica.jpeg', 'bahamas-nassau-downtown.jpg', 'BogotaNight.jpg', 'farol-da-barra-salvador.jpg']
-
-  end
+    @travel_pictures = ["costarica.jpeg", "bahamas-nassau-downtown.jpg", "BogotaNight.jpg", "farol-da-barra-salvador.jpg"]
+    end
 
   def about_params
-    @color = params[:color]
+    if params[:color] == nil
+      @color = 'orange'
+    else
+      @color = params[:color] 
+      end 
     @size = params[:shoe_size]
-  end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcom#index'
 
+  get 'index' => 'welcom#index'
+
   get 'params' => 'welcom#about_params'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
